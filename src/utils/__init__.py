24d25e0c256cd2b37eab2f1b78eef5803721373a
tagelmust273 <mega.tamu2@gmail.com>
2026-04-06 13:03:48 +0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,5 @@
-from .rate_limiter import RateLimiter
+"""Utilities module"""
 
-__all__ = ['RateLimiter']
+from .rate_limiter import RateLimiter, rate_limiter
+
+__all__ = ['RateLimiter', 'rate_limiter']
