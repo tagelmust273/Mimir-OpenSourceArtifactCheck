b7a644ab79da64796effe27e73be7d820e69d1f0
tagelmust273 <mega.tamu2@gmail.com>
2026-04-06 13:00:29 +0300

    Update __init__.py

--- src/security/__init__.py
+++ src/security/__init__.py
@@ -1,5 +1,12 @@
+"""Security module for validation, sanitization, and secure HTTP client"""
+
 from .validators import InputValidator
 from .sanitizers import OutputSanitizer
-from .http_client import SecureHTTPClient
+from .http_client import SecureHTTPClient, http_client
 
-__all__ = ['InputValidator', 'OutputSanitizer', 'SecureHTTPClient']
+__all__ = [
+    'InputValidator',
+    'OutputSanitizer',
+    'SecureHTTPClient',
+    'http_client'
+]
